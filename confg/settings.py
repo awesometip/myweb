e40a40e434aff63fe6edb5bacad9478f9a28e723
awesometip <younoksu@naver.com>
2022-02-21 14:17:36 +0000

    aa

--- confg/settings.py
+++ confg/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = '!0$&tse^076mg&k)lr&dil--i3syt0x-(hi=@$ccif*#7lp7u-'
 DEBUG = True
 
 #ALLOWED_HOSTS = ['34.64.161.44']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["34.64.161.44", "whatisgift.com", "django-container-tmyts.run.goorm.io"
+    
+]
 
 
 # Application definition
